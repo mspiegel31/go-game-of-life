d9e7af41a3972c2e5f75a2ecd6febd77a2f5f872
Mike Spiegel <mspiegel31@gmail.com>
2019-11-26 15:16:39 +0000

    stubbing out simulation

--- simulation.go
+++ simulation.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"fmt"
+)
 
 type simulationParams struct {
 	ticks       int
 	boardSize   int
 	renderDelay int
+}
+
+type coordinate struct {
+	i int
+	j int
+}
+
+type cell struct {
+	data     int
+	location coordinate
+	neigbors []coordinate
 }
 
 var DEFAULTS simulationParams = simulationParams{500, 30, 100}
@@ -24,7 +38,38 @@ func NewSimulationParams(args ...int) simulationParams {
 	return DEFAULTS
 }
 
-
 func runSimulation(params simulationParams) {
 	fmt.Printf("running simulation with params %#v", params)
+	board := initBoard(params.boardSize)
+	printBoard(board)
+}
+
+func initBoard(size int) [][]cell {
+	getRandomInt := func() int { return rand.Intn(2) }
+	board := make([][]cell, size)
+	for i := range board {
+		board[i] = make([]cell, size)
+		for j := range board[i] {
+			location := coordinate{i, j}
+			neighbors := identifyNeighbors(location, size)
+			board[i][j] = cell{getRandomInt(), location, neighbors}
+		}
+	}
+	return board
+}
+
+func identifyNeighbors(loc coordinate, size int) []coordinate {
+	return nil
+}
+
+
+func printBoard(board [][]cell) {
+	dataOnly:= make([][]int, len(board))
+	for i := range board {
+		for j:= range board[i] {
+			//FIXME: this is printing pointer values.  
+			dataOnly[i][j] = board[i][j].data
+		}
+	}
+	fmt.Print(dataOnly)
 }
